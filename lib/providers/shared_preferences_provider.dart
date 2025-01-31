60c69d72e49cb06875a52c3f47a28c0d6886c095
Sai Kumar <ksai44576@gmail.com>
2025-01-31 22:17:15 +0530

    Fix shared preferences provider to return SharedPreferencesAsync and update provider type

--- lib/providers/shared_preferences_provider.dart
+++ lib/providers/shared_preferences_provider.dart
@@ -5,6 +5,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 part 'shared_preferences_provider.g.dart';
 
 @Riverpod(keepAlive: true)
-FutureOr<SharedPreferencesAsync> sharedPreferences(Ref ref) {
-  throw UnimplementedError();
+SharedPreferencesAsync sharedPreferences(Ref ref) {
+  return SharedPreferencesAsync();
 }
--- lib/providers/shared_preferences_provider.g.dart
+++ lib/providers/shared_preferences_provider.g.dart
@@ -6,12 +6,11 @@ part of 'shared_preferences_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$sharedPreferencesHash() => r'7979f517c31882facdcb8429a31262a65ca3192b';
+String _$sharedPreferencesHash() => r'27f75883fdfa5699515101e124521cad2de328e6';
 
 /// See also [sharedPreferences].
 @ProviderFor(sharedPreferences)
-final sharedPreferencesProvider =
-    FutureProvider<SharedPreferencesAsync>.internal(
+final sharedPreferencesProvider = Provider<SharedPreferencesAsync>.internal(
   sharedPreferences,
   name: r'sharedPreferencesProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
@@ -23,6 +22,6 @@ final sharedPreferencesProvider =
 
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
-typedef SharedPreferencesRef = FutureProviderRef<SharedPreferencesAsync>;
+typedef SharedPreferencesRef = ProviderRef<SharedPreferencesAsync>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
