a3c6a6c6f3528e5d7fb03667112f0721d9351f21
Sai Kumar <ksai44576@gmail.com>
2025-02-01 19:22:09 +0530

    Add constants for handling global constants

---
+++ lib/core/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+@immutable
+final class Constants {
+  const Constants._();
+
+  static const String _keyPrefix = 'org.yominexus';
+  static const String themeModeKey = '$_keyPrefix.themeMode';
+}
