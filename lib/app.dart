4d1937ea932c94adf0bfe275b2858c8f0bf016ae
Sai Kumar <ksai44576@gmail.com>
2025-02-01 20:25:11 +0530

    Add routing constants for initial route and settings view

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:yominexus/core/constants.dart';
 
 class App extends ConsumerWidget {
   const App({super.key});
@@ -18,6 +19,8 @@ class App extends ConsumerWidget {
       supportedLocales: const <Locale>[
         Locale('en', ''),
       ],
+      routes: Constants.routes,
+      initialRoute: Constants.initialRoute,
     );
   }
 }
--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yominexus/ui/settings/settings_view.dart';
 
 @immutable
 final class Constants {
@@ -6,4 +7,9 @@ final class Constants {
 
   static const String _keyPrefix = 'org.yominexus';
   static const String themeModeKey = '$_keyPrefix.themeMode';
+
+  static const String initialRoute = SettingsView.routeName;
+  static final Map<String, WidgetBuilder> routes = {
+    SettingsView.routeName: (BuildContext context) => const SettingsView(),
+  };
 }
