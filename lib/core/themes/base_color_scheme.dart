ff4d40d45e9367d27632db5b0e552cf815f6e03f
Sai Kumar <ksai44576@gmail.com>
2025-02-01 21:27:21 +0530

    Add BaseColorScheme class for managing color schemes in dark and light modes

---
+++ lib/core/themes/base_color_scheme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+@immutable
+abstract class BaseColorScheme {
+  static const Color _surfaceContainer = Color(0xFF0C0C0C);
+  static const Color _surfaceContainerHigh = Color(0xFF131313);
+  static const Color _surfaceContainerHighest = Color(0xFF1B1B1B);
+
+  ColorScheme get dark;
+  ColorScheme get light;
+
+  ColorScheme getColorScheme({required bool isDark, required bool isAmoled}) {
+    if (!isDark) {
+      return light;
+    }
+
+    if (!isAmoled) {
+      return dark;
+    }
+
+    return dark.copyWith(
+      surface: Colors.black,
+      onSurface: Colors.white,
+      surfaceContainerHighest: _surfaceContainerHighest,
+      surfaceContainerLowest: _surfaceContainer,
+      surfaceContainerLow: _surfaceContainer,
+      surfaceContainer: _surfaceContainer,
+      surfaceContainerHigh: _surfaceContainerHigh,
+    );
+  }
+}
