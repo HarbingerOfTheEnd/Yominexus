26f2d244535ce240fdbe0faff24967735ed3fc91
Sai Kumar <ksai44576@gmail.com>
2025-02-02 15:17:59 +0530

    Add ColorScheme extension to SharedPreferences for color scheme retrieval

--- lib/core/extensions/shared_preferences.dart
+++ lib/core/extensions/shared_preferences.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:yominexus/core/constants.dart';
+import 'package:yominexus/core/themes/base_color_scheme.dart';
+import 'package:yominexus/core/themes/themes.dart';
+import 'package:yominexus/core/themes/yominexus_color_scheme.dart';
 
 extension ThemeModeExt on SharedPreferencesWithCache {
   ThemeMode getThemeMode() {
@@ -13,5 +16,20 @@ extension ThemeModeExt on SharedPreferencesWithCache {
 
   Future<void> setThemeMode(ThemeMode themeMode) async {
     await setString(Constants.themeModeKey, themeMode.name);
+  }
+}
+
+extension ColorSchemeExt on SharedPreferencesWithCache {
+  BaseColorScheme getColorScheme() {
+    final String colorScheme = getString(Constants.colorSchemeKey) ?? 'Default';
+    return colorSchemes.entries
+        .firstWhere(
+          (e) => e.key.name == colorScheme,
+          orElse: () => MapEntry(
+            YominexusTheme.yominexus,
+            YominexusColorScheme.instance,
+          ),
+        )
+        .value;
   }
 }
