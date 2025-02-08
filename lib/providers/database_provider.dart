61848bd044f84f9c17039ecd647d334b2665d606
Sai Kumar <ksai44576@gmail.com>
2025-02-08 23:15:50 +0530

    Add Riverpod database provider implementation

---
+++ lib/providers/database_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:yominexus/core/database/database.dart';
+
+part 'database_provider.g.dart';
+
+@riverpod
+Database database(Ref ref) {
+  throw UnimplementedError();
+}
