6f86b8331a26352443ba30baf10e20f51f5a1667
Sai Kumar <ksai44576@gmail.com>
2025-02-08 20:45:03 +0530

    Add extensions to BuildContext for theme and media query access

--- lib/core/extensions/build_context.dart
+++ lib/core/extensions/build_context.dart
@@ -4,3 +4,11 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 extension L10nExt on BuildContext {
   AppLocalizations get l10n => AppLocalizations.of(this)!;
 }
+
+extension ThemeExt on BuildContext {
+  ThemeData get theme => Theme.of(this);
+}
+
+extension MediaQueryExt on BuildContext {
+  MediaQueryData get mediaQuery => MediaQuery.of(this);
+}
