e1845c7595352d0f3da66954bd4f1817b1402858
Sai Kumar <ksai44576@gmail.com>
2025-02-23 15:58:45 +0530

    Move AppearanceView import to the correct directory structure

--- lib/ui/more/settings/settings_view.dart
+++ lib/ui/more/settings/settings_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:yominexus/core/extensions/build_context.dart';
-import 'package:yominexus/ui/more/settings/appearance_view.dart';
+import 'package:yominexus/ui/more/settings/appearance/appearance_view.dart';
 
 class SettingsView extends ConsumerWidget {
   static const String routeName = '/settings';
